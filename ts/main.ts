e6cb1b59ee07e804adcdc92bd1ed0ddb3316f157
Senire70 <senire70@gmail.com>
2024-04-12 09:08:33 -0400

    Correcciones semanticas en TS

--- ts/main.ts
+++ ts/main.ts
@@ -1,8 +1,8 @@
-let containers = document.querySelectorAll(".container");
+let containers:NodeListOf<Element> = document.querySelectorAll(".container");
 
-for (let i = 0; i < containers.length; i++) {
+for (let i:number = 0; i < containers.length; i++) {
     containers[i].addEventListener("click", (event) => {
-        for (let e = 0; e < containers.length; e++) {
+        for (let e:number = 0; e < containers.length; e++) {
             containers[e].classList.remove("selected");
             if (event.target !== null && event.target instanceof HTMLElement){
                 event.target.classList.add("selected");
